e3dce6274c5c66c6b1337c220e4e57a051544fe9
punjabi.gabru <dharmindersingh.gne@gmail.com>
2025-03-05 18:37:51 +0000

    utils code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 //require("dotenv").config({ path: ".env" });
-
+import { app } from "./app.js";
 import dotenv from "dotenv";
 dotenv.config({
     path: ".env",
 });
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+    .then(
+        app.listen(process.env.PORT || 4000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    )
+    .catch((error) => {
+        console.error("ERROR ", error);
+    });
 
 /*
 import express from "express";
