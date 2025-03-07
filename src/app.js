a5ca8a3b1691eb85c1b31226004b6f5f4517b491
punjabi.gabru <dharmindersingh.gne@gmail.com>
2025-03-07 17:19:09 +0000

    controllers,userrouter

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+const app = express();
 
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// CORS
 app.use(
     cors({
         origin: "process.env.CORS_ORIGIN",
         credentials: true,
     })
 );
-app.use(express.json({ limit: "20kb" }));
-app.use(express.urlencoded({ extended: true, limit: "20kb" }));
-app.use(express.static("public"));
-app.use(cookieParser());
 
-const app = express();
+// Routes............
+import userRouter from "./routes/user.route.js";
+
+//routes declarations................................
+app.use("/api/v1/users", userRouter);
 
 export { app };
