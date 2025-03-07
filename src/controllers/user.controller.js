3150550df9b216c61ed5014a245ea8b326ae91ec
punjabi.gabru <dharmindersingh.gne@gmail.com>
2025-03-07 17:48:37 +0000

    all setup configured

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
     return res.status(200).json({
-        message: "ok",
+        message: "well registered",
     });
 });
 
